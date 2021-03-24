fa743637fc43a2093cff8f63045e3513db9816c0
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 15:53:55 +0530

    Added:calculating length of line based on co-ordinates points

--- src/Line_Compare_Problem/Line_Comparison_Problem.java
+++ src/Line_Compare_Problem/Line_Comparison_Problem.java
@@ -5,15 +5,16 @@ public class Line_Comparison_Problem {
 	public static void main(String[] args) {
 
 		//Variables
-		int x1,x2,y1,y2;
-		int  line1;
-		//Taking Input
-		x1=2; y1=2;
-		x2=4;y2=4;
+		int x1=7;
+		int x2=2;
+		int y1=12;
+		int y2=10;
 
-		line1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		//calculating length of line base on co-ordinates points
+		int	lenght=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
 		//print Length of line
-		System.out.println("Length of Line is "+ line1);
+		System.out.println("Length of Line is "+ lenght);
 
 	}
 }
