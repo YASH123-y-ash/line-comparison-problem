a2940e3738f17b85f70a7f5f9a14fdb3bc43294c
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 07:09:37 +0530

    Added:checking equality of two lines using compareTo()

--- src/Line_Compare_Problem/Line_Comparison_Problem.java
+++ src/Line_Compare_Problem/Line_Comparison_Problem.java
@@ -6,25 +6,35 @@ public class Line_Comparison_Problem {
 
 		//Variables
 		int x1,x2,y1,y2,x3,y3;
-		int line1,line2;
+		int  line1,line2;
 
-		//initializing co-ordinates points
+		//initialize value
 		x1=2; y1=2;
 		x2=4;y2=4;
 		x3=5;y3=1;
-
+		
+		//computation of length of line
 		line1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		System.out.println("Line one value = " +line1);
-
 		line2=(int)Math.sqrt((x3-x1)*(x3-x1)+(y3-y1)*(y3-y1));
-		System.out.println("Line Two value = " +line2);
 
-		////converting the double into string and store the value String variable
-		String length1=String.valueOf(line1);
-		String length2=String.valueOf(line2);
+		//printing line1 and line2 value
+		System.out.println("Lenth of Line 1 value =" +line1);
+		System.out.println("Length od Line 2 value =" +line2);
 
-		//comparing to length if its  equal or Not
-		System.out.println(length1.equals(length2));
+		//creating a object of integer class
+		Integer length1=new Integer(line1);
+		Integer length2=new Integer(line2);
+
+		// comparing the lines length using compareTo()
+		int compare=length1.compareTo(length2);
+
+		if(compare>0) {
+			System.out.println("Line 1 is greater than Line 2");
+		}else if(compare<0) {
+			System.out.println("Line 1 is less than Line 2");
+		}else {
+			System.out.println("Line 1 is equal to Line 2");
+		}
 
 	}
 }
