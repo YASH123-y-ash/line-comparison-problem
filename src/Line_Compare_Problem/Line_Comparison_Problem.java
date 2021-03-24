f3531dfb75816dc879239c44fd13f2ee3d17ff9d
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 06:48:46 +0530

    Added:model a line based on co-ordinates and calculating its length

--- src/Line_Compare_Problem/Line_Comparison_Problem.java
+++ src/Line_Compare_Problem/Line_Comparison_Problem.java
@@ -3,6 +3,17 @@ package Line_Compare_Problem;
 public class Line_Comparison_Problem {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to line comparison problem");
+
+		//Variables
+		int x1,x2,y1,y2;
+		int  line1;
+		//Taking Input
+		x1=2; y1=2;
+		x2=4;y2=4;
+
+		line1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		//print Length of line
+		System.out.println("Length of Line is "+ line1);
+
 	}
 }
