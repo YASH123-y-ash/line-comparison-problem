11b8818c92ad6a3e68b40e22033212f297525139
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 06:59:31 +0530

    Added:checking the equality of two lines

--- src/Line_Compare_Problem/Line_Comparison_Problem.java
+++ src/Line_Compare_Problem/Line_Comparison_Problem.java
@@ -5,15 +5,26 @@ public class Line_Comparison_Problem {
 	public static void main(String[] args) {
 
 		//Variables
-		int x1,x2,y1,y2;
-		int  line1;
-		//Taking Input
+		int x1,x2,y1,y2,x3,y3;
+		int line1,line2;
+
+		//initializing co-ordinates points
 		x1=2; y1=2;
 		x2=4;y2=4;
+		x3=5;y3=1;
 
 		line1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		//print Length of line
-		System.out.println("Length of Line is "+ line1);
+		System.out.println("Line one value = " +line1);
+
+		line2=(int)Math.sqrt((x3-x1)*(x3-x1)+(y3-y1)*(y3-y1));
+		System.out.println("Line Two value = " +line2);
+
+		////converting the double into string and store the value String variable
+		String length1=String.valueOf(line1);
+		String length2=String.valueOf(line2);
+
+		//comparing to length if its  equal or Not
+		System.out.println(length1.equals(length2));
 
 	}
 }
