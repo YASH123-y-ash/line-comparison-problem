21ba583bab822be4d18ef9e2fe8c677a0a3fec85
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 06:42:09 +0530

    Added:welcome to line comparison problem

---
+++ src/Line_Compare_Problem/Line_Comparison_Problem.java
@@ -0,0 +1,8 @@
+package Line_Compare_Problem;
+
+public class Line_Comparison_Problem {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparison problem");
+	}
+}
